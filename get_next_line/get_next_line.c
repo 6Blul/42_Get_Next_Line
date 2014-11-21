76f502ea2a27aa7416cc025c06e50ac436b2c2e4
Sophie POCHEZ <spochez@e2r4p12.42.fr>
2014-11-21 16:10:46 +0100

    tu marches pas t'es qu'une pute

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,54 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: spochez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/20 16:03:55 by spochez           #+#    #+#             */
-/*   Updated: 2014/11/20 16:14:12 by spochez          ###   ########.fr       */
+/*   Created: 2014/11/21 15:14:20 by spochez           #+#    #+#             */
+/*   Updated: 2014/11/21 16:10:31 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/includes/libft.h"
 #include "get_next_line.h"
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int		ft_refresh(char **line, char **split, int i)
+int		ft_refresh(char **line, char **split)
 {
-	if (!split[i])
+	static int	j;
+
+	if (!split[j])
 		return (-1);
 	if (*line)
+	{
+		*line = NULL;
 		free (*line);
-	*line = ft_strdup(split[i]);
-	if (split[i + 1] == 0)
-		return (0);
-	return (1);
+	}
+	*line = (char *)malloc(sizeof(char) * (ft_strlen(split[j]) + 1));
+	*line = ft_strdup(split[j]);
+	if (split[j + 1])
+		return (1);
+	j++;
+	return (0);
 }
 
-int		get_next_line(int fd, char **line)
+int		get_next_line(int const fd, char **line)
 {
 	char		*buff;
-	static char	*str;
 	char		**split;
-	int			count;
-	static int	rd;
-	static int	stch;
+	static char	*res;
+	int			rd;
 	static int	i;
+	int			rf;
 
 	rd = 1;
-	count = 0;
-	buff = malloc(sizeof(char) * BUFF_SIZE + 1);
-	while (rd != 0 && stch != 1)
+	buff = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
+	while (rd != 0)
 	{
 		rd = read(fd, buff, BUFF_SIZE);
-		if (count == 0)
-			str = ft_strdup(buff);
+		if (i == 0)
+			res = ft_strdup(buff);
 		else
-			str = ft_strjoin(str, buff);
-		count++;
+			res = ft_strjoin(res, buff);
+		i++;
 	}
-	stch = 1;
-	if (buff == NULL || BUFF_SIZE == 0 || fd <= 0)
-		return (-1);
-	split = ft_strsplit(buff, '\n');
-	free (buff);
-	return (ft_refresh(&(*line), split, i++));
+	split = ft_strsplit(res, '\n');
+	rf = ft_refresh(&(*line), split);
+	free(buff);
+	return (rf);
 }
