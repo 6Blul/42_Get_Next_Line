a30e471af8d974101a0c15f74df2af3674533986
Sophie POCHEZ <spochez@e2r3p14.42.fr>
2014-11-23 16:57:34 +0100

    blbl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,58 +5,59 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: spochez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/21 15:14:20 by spochez           #+#    #+#             */
-/*   Updated: 2014/11/21 16:14:14 by spochez          ###   ########.fr       */
+/*   Created: 2014/11/18 18:43:33 by spochez           #+#    #+#             */
+/*   Updated: 2014/11/23 15:50:45 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/includes/libft.h"
 #include "get_next_line.h"
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int		ft_refresh(char **line, char **split)
+int		ft_refresh(char **line, char **split, int i)
 {
-	static int	j;
-
-	if (!split[j])
+	if (!split[i])
 		return (-1);
 	if (*line)
-	{
-		*line = NULL;
 		free (*line);
-	}
-	*line = ft_strdup(split[j]);
-	if (split[j + 1])
-	{
-		j++;
-		return (1);
-	}
-	return (0);
+	*line = ft_strdup(split[i]);
+	if (split[i + 1] == 0)
+		return (0);
+	return (1);
 }
 
-int		get_next_line(int const fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	char		*buff;
-	char		**split;
-	static char	*res;
-	int			rd;
-	static int	i;
-	int			rf;
+	char			*buff;
+	static char		*str;
+	static char		**split;
+	static int		i;
+	int				rd;
+	static int		stch;
+	static int		ct;
 
 	rd = 1;
-	buff = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
-	while (rd != 0)
+	buff = malloc(sizeof(char) * BUFF_SIZE + 1);
+	if (stch != 1)
 	{
-		rd = read(fd, buff, BUFF_SIZE);
-		if (i == 0)
-			res = ft_strdup(buff);
-		else
-			res = ft_strjoin(res, buff);
-		i++;
+		while (rd != 0)
+		{
+			rd = read(fd, buff, BUFF_SIZE);
+			if (ct == 0)
+				str = ft_strdup(buff);
+			else
+				str = ft_strjoin(str, buff);
+			ct++;
+		}
 	}
-	split = ft_strsplit(res, '\n');
-	rf = ft_refresh(&(*line), split);
-	free(buff);
-	return (rf);
+	//ft_putstr(str);
+	stch = 1;
+	if (str == NULL || BUFF_SIZE == 0 || fd <= 0)
+		return (-1);
+	split = ft_strsplit(str, '\n');
+	//while (*split)
+	//	ft_putendl(*split++);
+	free (buff);
+	return (ft_refresh(&(*line), split, i++));
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: spochez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 15:21:06 by spochez           #+#    #+#             */
-/*   Updated: 2014/11/19 19:04:07 by spochez          ###   ########.fr       */
+/*   Updated: 2014/11/23 15:41:27 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 14
+# define BUFF_SIZE 13
 
 int		get_next_line(int const fd, char **line);
 
