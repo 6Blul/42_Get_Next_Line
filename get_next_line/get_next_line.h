037ecce223e670aed8ab68250739d119cb9b2f66
Sophie POCHEZ <spochez@e2r3p8.42.fr>
2014-12-03 15:32:08 +0100

    testavances

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: spochez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 15:21:06 by spochez           #+#    #+#             */
-/*   Updated: 2014/11/30 06:43:56 by spochez          ###   ########.fr       */
+/*   Updated: 2014/12/03 15:14:58 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE -4
+# define BUFF_SIZE 5
 
 int		get_next_line(int const fd, char **line);
 
